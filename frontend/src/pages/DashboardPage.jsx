80e9ce137c8d5d931eb730ed7998c581f14df5bd
Aditya <arbeitaditya@gmail.com>
2025-08-28 15:10:01 +0700

    feat: add Table component to DashboardPage and remove test header

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import AdminNavbar from '../components/AdminNavbar'
 import Sidebar from '../components/Sidebar'
+import { Table } from '../components/Table'
 
 const DashboardPage = () => {
   return (
     <div className='min-h-screen mx-auto bg-background'>
-        <Sidebar />
+        <AdminNavbar />
 
-        <h1>test</h1>
+        <Table />
     </div>
   )
 }
